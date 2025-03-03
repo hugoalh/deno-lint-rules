3d92342765800ba4fac2bb13f53361b29d14438d
hugoalh <hugoalh@gmail.com>
2025-03-03 13:20:48 +0800

    Fix rule `prefer-regexp-flag-unicode` is not working

--- rules/prefer_regexp_flag_unicode.ts
+++ rules/prefer_regexp_flag_unicode.ts
@@ -2,18 +2,24 @@ import type { DenoLintRuleDataPre } from "../_utility.ts";
 const ruleContextStatic: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
-			RegExpLiteral(node: Deno.lint.RegExpLiteral): void {
-				const flags: readonly string[] = node.regex.flags.split("");
-				if (!flags.includes("u") && !flags.includes("v")) {
-					const regexpExpect: string = `/${node.regex.pattern}/${node.regex.flags}u`;
-					context.report({
-						range: node.range,
-						message: `Prefer the regular expression is contain Unicode flag (\`u\` or \`v\`).`,
-						hint: `Do you mean \`${regexpExpect}\`?`,
-						fix(fixer: Deno.lint.Fixer): Deno.lint.FixData {
-							return fixer.replaceText(node, regexpExpect);
-						}
-					});
+			Literal(node: Deno.lint.Literal): void {
+				if (node.value instanceof RegExp) {
+					const flags: readonly string[] = (node as Deno.lint.RegExpLiteral).regex.flags.split("");
+					if (!flags.includes("u") && !flags.includes("v")) {
+						const regexpExpectU: string = `/${(node as Deno.lint.RegExpLiteral).regex.pattern}/${(node as Deno.lint.RegExpLiteral).regex.flags}u`;
+						const regexpExpectV: string = `/${(node as Deno.lint.RegExpLiteral).regex.pattern}/${(node as Deno.lint.RegExpLiteral).regex.flags}v`;
+						context.report({
+							node,
+							message: `Prefer the regular expression is contain Unicode flag (\`u\` or \`v\`).`,
+							hint: `Do you mean \`${regexpExpectU}\` or \`${regexpExpectV}\`?`,
+							fix(fixer: Deno.lint.Fixer): Iterable<Deno.lint.FixData> {
+								return [
+									fixer.replaceText(node, regexpExpectU),
+									fixer.replaceText(node, regexpExpectV)
+								];
+							}
+						});
+					}
 				}
 			}
 		};
