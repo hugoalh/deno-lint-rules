0f928091c6b1625acb9f7bd4f4dd8f0b489fabaa
hugoalh <hugoalh@gmail.com>
2025-03-05 18:42:26 +0800

    Implement NPM module import rewrite

--- rules/no_import_npm.ts
+++ rules/no_import_npm.ts
@@ -11,18 +11,40 @@ export interface DenoLintRuleNoImportNPMOptions {
 	 */
 	viaURL?: boolean;
 }
-const regexpNPMURLs: readonly RegExp[] = [
-	/^https?:\/\/(?:dev\.|ga\.)?jspm\.io\/npm:/,
-	/^https?:\/\/(?:cdn\.)?(?:pika|skypack)\.dev/,
-	/^https?:\/\/esm\.sh\/(?!jsr\/|gh\/|pr\/|pkg\.pr\.new\/)/,
-	/^https?:\/\/unpkg\.com\//,
-	/^https?:\/\/cdn\.jsdelivr\.net\/npm\//,
-	/^https?:\/\/esm\.run\//
-];
-function isNPMURL(item: string): boolean {
-	return regexpNPMURLs.some((regexpNPMURL: RegExp): boolean => {
-		return regexpNPMURL.test(item);
-	});
+const regexpNPMURLESMRun = /^https?:\/\/esm\.run\//;
+const regexpNPMURLESMsh = /^https?:\/\/esm\.sh\/(?!jsr\/|gh\/|pr\/|pkg\.pr\.new\/)/;
+const regexpNPMURLJSDelivr = /^https?:\/\/cdn\.jsdelivr\.net\/npm\//;
+const regexpNPMURLJSPM = /^https?:\/\/(?:dev\.|ga\.)?jspm\.io\/npm:/;
+const regexpNPMURLPika = /^https?:\/\/cdn\.(?:pika|skypack)\.dev/;
+const regexpNPMURLUnpkg = /^https?:\/\/unpkg\.com\//;
+function resolveNPMImportFromURL(item: string): boolean | string {
+	if (
+		regexpNPMURLESMRun.test(item) ||
+		regexpNPMURLESMsh.test(item) ||
+		regexpNPMURLPika.test(item) ||
+		regexpNPMURLUnpkg.test(item)
+	) {
+		try {
+			return `npm:${new URL(item).pathname.slice(1)}`;
+		} catch {
+			return true;
+		}
+	}
+	if (regexpNPMURLJSDelivr.test(item)) {
+		try {
+			return `npm:${new URL(item).pathname.slice(5)}`;
+		} catch {
+			return true;
+		}
+	}
+	if (regexpNPMURLJSPM.test(item)) {
+		try {
+			return new URL(item).pathname.slice(1).replace(/\/$/, "");
+		} catch {
+			return true;
+		}
+	}
+	return false;
 }
 const ruleMessageProtocol = `Import NPM module via protocol \`npm:\` is forbidden.`;
 const ruleMessageURL = `Import NPM module via URL is forbidden.`;
@@ -43,11 +65,21 @@ export const data: DenoLintRuleDataPre<DenoLintRuleNoImportNPMOptions> = {
 								message: ruleMessageProtocol
 							});
 						}
-						if (viaURL && isNPMURL(node.source.value)) {
-							context.report({
-								node: node.source,
-								message: ruleMessageURL
-							});
+						if (viaURL) {
+							const result: boolean | string = resolveNPMImportFromURL(node.source.value);
+							if (
+								(typeof result === "boolean" && result) ||
+								typeof result === "string"
+							) {
+								context.report({
+									node: node.source,
+									message: ruleMessageURL,
+									hint: (typeof result === "string" && !viaProtocol) ? `Do you mean to import \`${result}\`?` : undefined,
+									fix: (typeof result === "string" && !viaProtocol) ? ((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
+										return fixer.replaceText(node.source, node.source.raw.replace(node.source.value, result));
+									}) : undefined
+								});
+							}
 						}
 					},
 					ExportNamedDeclaration(node: Deno.lint.ExportNamedDeclaration): void {
@@ -58,11 +90,21 @@ export const data: DenoLintRuleDataPre<DenoLintRuleNoImportNPMOptions> = {
 									message: ruleMessageProtocol
 								});
 							}
-							if (viaURL && isNPMURL(node.source.value)) {
-								context.report({
-									node: node.source,
-									message: ruleMessageURL
-								});
+							if (viaURL) {
+								const result: boolean | string = resolveNPMImportFromURL(node.source.value);
+								if (
+									(typeof result === "boolean" && result) ||
+									typeof result === "string"
+								) {
+									context.report({
+										node: node.source,
+										message: ruleMessageURL,
+										hint: (typeof result === "string" && !viaProtocol) ? `Do you mean to import \`${result}\`?` : undefined,
+										fix: (typeof result === "string" && !viaProtocol) ? ((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
+											return fixer.replaceText(node.source!, node.source!.raw.replace(node.source!.value, result));
+										}) : undefined
+									});
+								}
 							}
 						}
 					},
@@ -73,11 +115,21 @@ export const data: DenoLintRuleDataPre<DenoLintRuleNoImportNPMOptions> = {
 								message: ruleMessageProtocol
 							});
 						}
-						if (viaURL && isNPMURL(node.source.value)) {
-							context.report({
-								node: node.source,
-								message: ruleMessageURL
-							});
+						if (viaURL) {
+							const result: boolean | string = resolveNPMImportFromURL(node.source.value);
+							if (
+								(typeof result === "boolean" && result) ||
+								typeof result === "string"
+							) {
+								context.report({
+									node: node.source,
+									message: ruleMessageURL,
+									hint: (typeof result === "string" && !viaProtocol) ? `Do you mean to import \`${result}\`?` : undefined,
+									fix: (typeof result === "string" && !viaProtocol) ? ((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
+										return fixer.replaceText(node.source, node.source.raw.replace(node.source.value, result));
+									}) : undefined
+								});
+							}
 						}
 					},
 					ImportExpression(node: Deno.lint.ImportExpression): void {
@@ -88,11 +140,21 @@ export const data: DenoLintRuleDataPre<DenoLintRuleNoImportNPMOptions> = {
 									message: ruleMessageProtocol
 								});
 							}
-							if (viaURL && isNPMURL(node.source.value)) {
-								context.report({
-									node: node.source,
-									message: ruleMessageURL
-								});
+							if (viaURL) {
+								const result: boolean | string = resolveNPMImportFromURL(node.source.value);
+								if (
+									(typeof result === "boolean" && result) ||
+									typeof result === "string"
+								) {
+									context.report({
+										node: node.source,
+										message: ruleMessageURL,
+										hint: (typeof result === "string" && !viaProtocol) ? `Do you mean to import \`${result}\`?` : undefined,
+										fix: (typeof result === "string" && !viaProtocol) ? ((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
+											return fixer.replaceText(node.source, (node.source as Deno.lint.StringLiteral).raw.replace((node.source as Deno.lint.StringLiteral).value, result));
+										}) : undefined
+									});
+								}
 							}
 						}
 					}
