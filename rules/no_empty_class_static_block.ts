796d6c4e19aed3a136402590a454ba8fdb3b73dd
hugoalh <hugoalh@gmail.com>
2025-03-05 17:07:09 +0800

    Improve rule `no-empty-class-static-block` message

--- rules/no_empty_class_static_block.ts
+++ rules/no_empty_class_static_block.ts
@@ -10,7 +10,7 @@ const ruleContextStatic: Deno.lint.Rule = {
 					if ((context.sourceCode.text.slice(blockBegin + 1, blockEnd - 1)).trim().length === 0) {
 						context.report({
 							node,
-							message: `Empty class static block is forbidden. While not technically errors, usually occur due to refactoring that was not completed, which cause confusion when reading code.`,
+							message: `Empty class static block is forbidden. While not technically errors, which cause confusion when reading code.`,
 							fix(fixer: Deno.lint.Fixer): Deno.lint.Fix {
 								return fixer.remove(node);
 							}
