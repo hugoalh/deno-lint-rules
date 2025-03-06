d057ae3bb53d8936ffd659a17d945a30083bc310
hugoalh <hugoalh@gmail.com>
2025-03-06 12:58:16 +0800

    Make rule `no-ternary-boolean` fixer support NOT condition

--- rules/no_ternary_boolean.ts
+++ rules/no_ternary_boolean.ts
@@ -1,16 +1,27 @@
 import type { DenoLintRuleDataPre } from "../_template.ts";
+const ruleMessage = `Boolean ternaries are pointless hence forbidden.`;
 const ruleContextStatic: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
 			ConditionalExpression(node: Deno.lint.ConditionalExpression): void {
-				if (node.consequent.type === "Literal" && node.consequent.value === true && node.alternate.type === "Literal" && node.alternate.value === false) {
-					context.report({
-						node,
-						message: `Boolean ternaries are pointless hence forbidden.`,
-						fix(fixer: Deno.lint.Fixer): Deno.lint.Fix {
-							return fixer.replaceText(node, context.sourceCode.getText(node.test));
-						}
-					});
+				if (node.consequent.type === "Literal" && node.alternate.type === "Literal") {
+					if (node.consequent.value === true && node.alternate.value === false) {
+						context.report({
+							node,
+							message: ruleMessage,
+							fix(fixer: Deno.lint.Fixer): Deno.lint.Fix {
+								return fixer.replaceText(node, context.sourceCode.getText(node.test));
+							}
+						});
+					} else if (node.consequent.value === false && node.alternate.value === true) {
+						context.report({
+							node,
+							message: ruleMessage,
+							fix(fixer: Deno.lint.Fixer): Deno.lint.Fix {
+								return fixer.replaceText(node, `!(${context.sourceCode.getText(node.test)})`);
+							}
+						});
+					}
 				}
 			}
 		};
