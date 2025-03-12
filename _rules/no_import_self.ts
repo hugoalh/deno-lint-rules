fd645ba1b9f7a081bf8230b37b8d50de6a0da47e
hugoalh <hugoalh@gmail.com>
2025-03-12 17:38:54 +0800

    Improve rule `no-import-self` detection

--- _rules/no_import_self.ts
+++ _rules/no_import_self.ts
@@ -1,18 +1,49 @@
-import { dirname as getPathDirname } from "jsr:@std/path@^1.0.8/dirname";
 import { basename as getPathBasename } from "jsr:@std/path@^1.0.8/basename";
+import { dirname as getPathDirname } from "jsr:@std/path@^1.0.8/dirname";
+import { fromFileUrl as getPathFromFileUrl } from "jsr:@std/path@^1.0.8/from-file-url";
 import { resolve as resolvePath } from "jsr:@std/path@^1.0.8/resolve";
+import { toFileUrl as convertPathToFileUrl } from "jsr:@std/path@^1.0.8/to-file-url";
 import type { DenoLintRuleDataPre } from "../_template.ts";
 import { isStringLiteral } from "../_utility.ts";
+function isImportFromFileUrlLike(pattern: string, source: string): boolean {
+	return (
+		source === pattern ||
+		(source.startsWith(pattern) && (
+			source.charAt(pattern.length) === "#" ||
+			source.charAt(pattern.length) === "?"
+		))
+	);
+}
 function ruleAssertor(context: Deno.lint.RuleContext, source: Deno.lint.StringLiteral): void {
-	if (
-		source.value === `./${getPathBasename(context.filename)}` ||
-		context.filename === resolvePath(getPathDirname(context.filename), source.value)
-	) {
-		context.report({
-			node: source,
-			message: `Import self as module is forbidden.`
-		});
+	//deno-lint-ignore hugoalh/no-useless-try
+	try {
+		const contextFileRelative: string = `./${getPathBasename(context.filename)}`;
+		const contextFileUrl: string = convertPathToFileUrl(context.filename).href;
+		let result: boolean = false;
+		if (isImportFromFileUrlLike(contextFileRelative, source.value)) {
+			result = true;
+		} else if (source.value.startsWith(".")) {
+			if (isImportFromFileUrlLike(contextFileUrl, convertPathToFileUrl(resolvePath(getPathDirname(context.filename), source.value)).href)) {
+				result = true;
+			}
+		} else if (source.value.startsWith("/")) {
+			if (isImportFromFileUrlLike(contextFileUrl, convertPathToFileUrl(resolvePath(source.value)).href)) {
+				result = true;
+			}
+		} else if (source.value.startsWith("file:")) {
+			if (isImportFromFileUrlLike(contextFileUrl, convertPathToFileUrl(getPathFromFileUrl(source.value)).href)) {
+				result = true;
+			}
+		}
+		if (result) {
+			context.report({
+				node: source,
+				message: `Import self as module is forbidden.`
+			});
+		}
 	}
+	//deno-lint-ignore no-empty -- Continue on error.
+	catch { }
 }
 const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
