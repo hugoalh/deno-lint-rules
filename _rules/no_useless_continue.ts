b643e26d6f3c916be731474556512812daccc05d
hugoalh <hugoalh@gmail.com>
2025-03-17 18:24:45 +0800

    Fmt

--- _rules/no_useless_continue.ts
+++ _rules/no_useless_continue.ts
@@ -1,13 +1,13 @@
 import type { DenoLintRuleDataPre } from "../_template.ts";
 function ruleAssertor(context: Deno.lint.RuleContext, node: Deno.lint.DoWhileStatement | Deno.lint.ForInStatement | Deno.lint.ForOfStatement | Deno.lint.ForStatement | Deno.lint.WhileStatement): void {
 	if (node.body.type === "BlockStatement") {
-		const lastStatement: Deno.lint.Statement = node.body.body[node.body.body.length - 1];
-		if (lastStatement.type === "ContinueStatement" && lastStatement.label === null) {
+		const blockLastStatement: Deno.lint.Statement = node.body.body[node.body.body.length - 1];
+		if (blockLastStatement.type === "ContinueStatement" && blockLastStatement.label === null) {
 			context.report({
-				node: lastStatement,
+				node: blockLastStatement,
 				message: `The statement \`continue\` at the end of the loop statement and without label is useless.`,
 				fix(fixer: Deno.lint.Fixer): Deno.lint.Fix {
-					return fixer.remove(lastStatement);
+					return fixer.remove(blockLastStatement);
 				}
 			});
 		}
