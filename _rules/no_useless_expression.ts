dfc9dd856332b0e2f2045451a0c3843997dad799
hugoalh <hugoalh@gmail.com>
2025-03-17 17:54:31 +0800

    Remove rule `no-useless-expression` fixer

--- _rules/no_useless_expression.ts
+++ _rules/no_useless_expression.ts
@@ -1,29 +1,17 @@
 import type { DenoLintRuleDataPre } from "../_template.ts";
-const ruleMessage = `The expression will do nothing, likely missing the assignment or call.`;
 const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
 			ExpressionStatement(node: Deno.lint.ExpressionStatement): void {
-				switch (node.expression.type) {
-					case "Identifier":
-						context.report({
-							node,
-							message: ruleMessage
-							// NOTE: Fixer is temporary disabled to prevent accidentally broken the script.
-							/*,
-							fix(fixer: Deno.lint.Fixer): Deno.lint.Fix {
-								return fixer.remove(node);
-							}
-							*/
-						});
-						break;
-					case "Literal":
-						context.report({
-							node,
-							message: ruleMessage
-							// NOTE: No fixer for literal in order to prevent accidentally removed important data.
-						});
-						break;
+				if ((
+					node.expression.type === "Identifier" ||
+					node.expression.type === "Literal"
+				)) {
+					context.report({
+						node,
+						message: `The expression will do nothing, likely missing the assignment or call.`
+						// NOTE: No fixer in order to prevent accidentally broken the script or removed important data.
+					});
 				}
 			}
 		};
