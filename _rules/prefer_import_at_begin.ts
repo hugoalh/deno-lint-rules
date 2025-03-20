c2f2a777d28d1d37229645576355168f858d64bb
hugoalh <hugoalh@gmail.com>
2025-03-20 11:06:07 +0800

    Add last valid position hint for rule `prefer-import-at-begin`

--- _rules/prefer_import_at_begin.ts
+++ _rules/prefer_import_at_begin.ts
@@ -1,19 +1,39 @@
 import type { DenoLintRuleDataPre } from "../_template.ts";
+import {
+	getContextPosition,
+	type ContextPosition
+} from "../_utility.ts";
 const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
 			Program(node: Deno.lint.Program): void {
-				let doneValidImports: boolean = false;
+				let done: boolean = false;
+				let last: Deno.lint.ImportDeclaration;
 				for (const body of node.body) {
 					if (body.type === "ImportDeclaration") {
-						if (doneValidImports) {
-							context.report({
+						if (done) {
+							const report: Deno.lint.ReportData = {
 								node: body,
 								message: `Prefer statements \`import\` at the begin of the file.`
-							});
+							};
+							//deno-lint-ignore hugoalh/no-useless-try
+							try {
+								const {
+									columnBegin,
+									columnEnd,
+									lineBegin,
+									lineEnd
+								}: ContextPosition = getContextPosition(context, last!);
+								report.hint = `Last valid import declaration locate at range from line ${lineBegin} column ${columnBegin} to line ${lineEnd} column ${columnEnd}.`;
+							}
+							//deno-lint-ignore no-empty -- Continue on error.
+							catch { }
+							context.report(report);
+						} else {
+							last = body;
 						}
 					} else {
-						doneValidImports = true;
+						done = true;
 					}
 				}
 			}
