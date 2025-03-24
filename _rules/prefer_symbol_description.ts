44523dd6936535bf92db17041486c74974342608
hugoalh <hugoalh@gmail.com>
2025-03-24 16:10:04 +0800

    Fmt

--- _rules/prefer_symbol_description.ts
+++ _rules/prefer_symbol_description.ts
@@ -3,7 +3,33 @@ const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
 			CallExpression(node: Deno.lint.CallExpression): void {
-				if (node.callee.type === "Identifier" && node.callee.name === "Symbol" && node.arguments.length === 0) {
+				if (node.callee.type === "Identifier" && node.callee.name === "Symbol" && (
+					node.arguments.length === 0 ||
+					// undefined
+					(node.arguments[0].type === "Identifier" && node.arguments[0].name === "undefined") ||
+					(node.arguments[0].type === "MemberExpression" && (
+						// globalThis.undefined / globalThis["undefined"] / window.undefined / window["undefined"]
+						(
+							node.arguments[0].object.type === "Identifier" && (
+								node.arguments[0].object.name === "globalThis" ||
+								node.arguments[0].object.name === "window"
+							) && (
+								(node.arguments[0].property.type === "Identifier" && node.arguments[0].property.name === "undefined") ||
+								(node.arguments[0].property.type === "Literal" && node.arguments[0].property.value === "undefined")
+							)
+						) ||
+						// globalThis.window.undefined / globalThis.window["undefined"] / globalThis["window"].undefined / globalThis["window"]["undefined"]
+						(
+							node.arguments[0].object.type === "MemberExpression" && node.arguments[0].object.object.type === "Identifier" && node.arguments[0].object.object.name === "globalThis" && (
+								(node.arguments[0].object.property.type === "Identifier" && node.arguments[0].object.property.name === "window") ||
+								(node.arguments[0].object.property.type === "Literal" && node.arguments[0].object.property.value === "window")
+							) && (
+								(node.arguments[0].property.type === "Identifier" && node.arguments[0].property.name === "undefined") ||
+								(node.arguments[0].property.type === "Literal" && node.arguments[0].property.value === "undefined")
+							)
+						)
+					))
+				)) {
 					context.report({
 						node,
 						message: `Prefer \`Symbol\` to have a description.`
