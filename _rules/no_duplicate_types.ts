499479758c6c353712068767faa3dfee77b03955
hugoalh <hugoalh@gmail.com>
2025-03-24 17:37:35 +0800

    Improve rules `no-duplicate-interfaces` and `no-duplicate-types` detection

--- _rules/no_duplicate_types.ts
+++ _rules/no_duplicate_types.ts
@@ -4,45 +4,51 @@ import {
 	standardizeNode,
 	type ContextPosition
 } from "../_utility.ts";
+function ruleAssertor(context: Deno.lint.RuleContext, statements: readonly Deno.lint.Statement[]): void {
+	const entries: Record<string, Deno.lint.TSTypeAliasDeclaration[]> = {};
+	function addEntry(node: Deno.lint.TSTypeAliasDeclaration): void {
+		const nodeAnnotationStandardize: string = standardizeNode(node.typeAnnotation);
+		entries[nodeAnnotationStandardize] ??= [];
+		entries[nodeAnnotationStandardize].push(node);
+	}
+	for (const statement of statements) {
+		if (statement.type === "ExportNamedDeclaration" && statement.declaration?.type === "TSTypeAliasDeclaration") {
+			// export type
+			addEntry(statement.declaration);
+		} else if (statement.type === "TSTypeAliasDeclaration") {
+			// type
+			addEntry(statement);
+		}
+	}
+	for (const entryNodes of Object.values(entries)) {
+		if (entryNodes.length > 1) {
+			const entryNodesPosition: readonly string[] = entryNodes.map((entryNode: Deno.lint.TSTypeAliasDeclaration): string => {
+				const {
+					columnBegin,
+					columnEnd,
+					lineBegin,
+					lineEnd
+				}: ContextPosition = getContextPosition(context, entryNode);
+				return `- Line ${lineBegin} Column ${columnBegin} ~ Line ${lineEnd} Column ${columnEnd}`;
+			});
+			for (let index: number = 0; index < entryNodes.length; index += 1) {
+				context.report({
+					node: entryNodes[index],
+					message: `Found duplicate type aliases, possibly mergeable.`,
+					hint: `Position of other duplicated type aliases:\n${entryNodesPosition.toSpliced(index, 1).join("\n")}`
+				});
+			}
+		}
+	}
+}
 const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
+			BlockStatement(node: Deno.lint.BlockStatement): void {
+				ruleAssertor(context, node.body);
+			},
 			Program(node: Deno.lint.Program): void {
-				const entries: Record<string, Deno.lint.TSTypeAliasDeclaration[]> = {};
-				function addEntry(node: Deno.lint.TSTypeAliasDeclaration): void {
-					const nodeAnnotationStandardize: string = standardizeNode(node.typeAnnotation);
-					entries[nodeAnnotationStandardize] ??= [];
-					entries[nodeAnnotationStandardize].push(node);
-				}
-				for (const statement of node.body) {
-					if (statement.type === "ExportNamedDeclaration" && statement.declaration?.type === "TSTypeAliasDeclaration") {
-						// export type
-						addEntry(statement.declaration);
-					} else if (statement.type === "TSTypeAliasDeclaration") {
-						// type
-						addEntry(statement);
-					}
-				}
-				for (const entryNodes of Object.values(entries)) {
-					if (entryNodes.length > 1) {
-						const entryNodesPosition: readonly string[] = entryNodes.map((entryNode: Deno.lint.TSTypeAliasDeclaration): string => {
-							const {
-								columnBegin,
-								columnEnd,
-								lineBegin,
-								lineEnd
-							}: ContextPosition = getContextPosition(context, entryNode);
-							return `- Line ${lineBegin} Column ${columnBegin} ~ Line ${lineEnd} Column ${columnEnd}`;
-						});
-						for (let index: number = 0; index < entryNodes.length; index += 1) {
-							context.report({
-								node: entryNodes[index],
-								message: `Found duplicate type aliases, possibly mergeable.`,
-								hint: `Position of other duplicated type aliases:\n${entryNodesPosition.toSpliced(index, 1).join("\n")}`
-							});
-						}
-					}
-				}
+				ruleAssertor(context, node.body);
 			}
 		};
 	}
