42591dd5d348213d2edf7a3d83f6a5e1a955e5e1
hugoalh <hugoalh@gmail.com>
2025-03-26 17:44:04 +0800

    Improve rule `no-if-return-else` detection

--- _rules/no_if_return_else.ts
+++ _rules/no_if_return_else.ts
@@ -3,14 +3,27 @@ const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
 			IfStatement(node: Deno.lint.IfStatement): void {
-				if (node.alternate !== null && (
-					(node.consequent.type === "BlockStatement" && node.consequent.body[node.consequent.body.length - 1].type === "ReturnStatement") ||
-					node.consequent.type === "ReturnStatement"
-				)) {
-					context.report({
-						node,
-						message: `The statement \`if\` has the return statement at the end, thus the statement \`else\` become unnecessary.`
-					});
+				if (node.alternate !== null) {
+					switch (((node.consequent.type === "BlockStatement") ? node.consequent.body[node.consequent.body.length - 1] : node.consequent).type) {
+						case "BreakStatement":
+							context.report({
+								node,
+								message: `The statement \`if\` has the statement \`break\` at the end, thus the statement \`else\` become unnecessary.`
+							});
+							break;
+						case "ContinueStatement":
+							context.report({
+								node,
+								message: `The statement \`if\` has the statement \`continue\` at the end, thus the statement \`else\` become unnecessary.`
+							});
+							break;
+						case "ReturnStatement":
+							context.report({
+								node,
+								message: `The statement \`if\` has the statement \`return\` at the end, thus the statement \`else\` become unnecessary.`
+							});
+							break;
+					}
 				}
 			}
 		};
