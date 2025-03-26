47de96bee3299602dfb4a1c77ba60b96716adbf4
hugoalh <hugoalh@gmail.com>
2025-03-26 17:52:33 +0800

    Fmt

--- _rules/no_useless_try.ts
+++ _rules/no_useless_try.ts
@@ -3,13 +3,13 @@ const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
 			TryStatement(node: Deno.lint.TryStatement): void {
-				const hasCatch: boolean = node.handler !== null;
-				const isCatchUseless: boolean = hasCatch && (
+				const isCatchExist: boolean = node.handler !== null;
+				const isCatchUseless: boolean = isCatchExist && (
 					node.handler!.body.body.length === 0 ||
 					(node.handler!.param?.type === "Identifier" && node.handler!.body.body[0].type === "ThrowStatement" && node.handler!.body.body[0].argument.type === "Identifier" && node.handler!.body.body[0].argument.name === node.handler!.param.name)
 				);
-				const hasFinally: boolean = node.finalizer !== null;
-				const isFinallyUseless: boolean = hasFinally && node.finalizer!.body.length === 0;
+				const isFinallyExist: boolean = node.finalizer !== null;
+				const isFinallyUseless: boolean = isFinallyExist && node.finalizer!.body.length === 0;
 				if (isCatchUseless && isFinallyUseless) {
 					context.report({
 						node,
@@ -20,18 +20,18 @@ const ruleContext: Deno.lint.Rule = {
 					});
 				} else if (isCatchUseless) {
 					context.report({
-						node: hasFinally ? node.handler! : node,
+						node: isFinallyExist ? node.handler! : node,
 						message: `The statement \`try-catch\` is useless.`,
 						fix(fixer: Deno.lint.Fixer): Deno.lint.Fix {
-							return (hasFinally ? fixer.remove(node.handler!) : fixer.replaceText(node, context.sourceCode.getText(node.block)));
+							return (isFinallyExist ? fixer.remove(node.handler!) : fixer.replaceText(node, context.sourceCode.getText(node.block)));
 						}
 					});
 				} else if (isFinallyUseless) {
 					context.report({
-						node: hasCatch ? node.finalizer! : node,
+						node: isCatchExist ? node.finalizer! : node,
 						message: `The statement \`try-finally\` is useless.`,
 						fix(fixer: Deno.lint.Fixer): Deno.lint.Fix {
-							return (hasCatch ? fixer.remove(node.finalizer!) : fixer.replaceText(node, context.sourceCode.getText(node.block)));
+							return (isCatchExist ? fixer.remove(node.finalizer!) : fixer.replaceText(node, context.sourceCode.getText(node.block)));
 						}
 					});
 				}
