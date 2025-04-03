f7c58482eb163ce393707679dc70ee53b66cc78f
hugoalh <hugoalh@gmail.com>
2025-04-03 17:57:39 +0800

    Bump version

--- .hugoalh.lint.ts
+++ .hugoalh.lint.ts
@@ -5,7 +5,6 @@ export default configureDenoLintPlugin({
 	"no-confirm": true,
 	"no-iife": true,
 	"no-import-dynamic": true,
-	"no-import-node": true,
 	"no-import-npm": true,
 	"no-prompt": true
 }) satisfies Deno.lint.Plugin as Deno.lint.Plugin;
