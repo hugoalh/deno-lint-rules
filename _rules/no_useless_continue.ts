b99506e18d015e1bdaaebc089b2113dd27dfd7d9
hugoalh <hugoalh@gmail.com>
2025-04-24 17:25:34 +0800

    Improve rule `no-useless-continue` detection

--- _rules/no_useless_continue.ts
+++ _rules/no_useless_continue.ts
@@ -1,16 +1,25 @@
 import type { DenoLintRuleDataPre } from "../_template.ts";
+const ruleMessage = `The statement \`continue\` at the end of the loop statement and without label is useless.`;
 function ruleAssertor(context: Deno.lint.RuleContext, node: Deno.lint.DoWhileStatement | Deno.lint.ForInStatement | Deno.lint.ForOfStatement | Deno.lint.ForStatement | Deno.lint.WhileStatement): void {
-	if (node.body.type === "BlockStatement") {
+	if (node.body.type === "BlockStatement" && node.body.body.length > 0) {
 		const blockLastStatement: Deno.lint.Statement = node.body.body[node.body.body.length - 1];
 		if (blockLastStatement.type === "ContinueStatement" && blockLastStatement.label === null) {
 			context.report({
 				node: blockLastStatement,
-				message: `The statement \`continue\` at the end of the loop statement and without label is useless.`,
+				message: ruleMessage,
 				fix(fixer: Deno.lint.Fixer): Deno.lint.Fix | Iterable<Deno.lint.Fix> {
 					return fixer.remove(blockLastStatement);
 				}
 			});
 		}
+	} else if (node.body.type === "ContinueStatement" && node.body.label === null) {
+		context.report({
+			node,
+			message: ruleMessage,
+			fix(fixer: Deno.lint.Fixer): Deno.lint.Fix | Iterable<Deno.lint.Fix> {
+				return fixer.remove(node);
+			}
+		});
 	}
 }
 const ruleContext: Deno.lint.Rule = {
