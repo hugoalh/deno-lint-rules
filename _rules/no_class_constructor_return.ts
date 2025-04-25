def78272053dd6ea9814fcf1b0b6a4aee9030b99
hugoalh <hugoalh@gmail.com>
2025-04-25 13:06:28 +0800

    Fix rule `no-class-constructor-return` selector

--- _rules/no_class_constructor_return.ts
+++ _rules/no_class_constructor_return.ts
@@ -2,15 +2,35 @@ import type { DenoLintRuleData } from "../_utility.ts";
 const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
-			"MethodDefinition[kind=\"constructor\"] ReturnStatement"(node: Deno.lint.ReturnStatement): void {
+			ReturnStatement(node: Deno.lint.ReturnStatement): void {
 				if (node.argument !== null) {
-					context.report({
-						node,
-						message: `Return value in the class constructor is possibly mistake.`,
-						fix(fixer: Deno.lint.Fixer): Deno.lint.Fix | Iterable<Deno.lint.Fix> {
-							return fixer.remove(node.argument!);
+					const ancestors: readonly Deno.lint.Node[] = context.sourceCode.getAncestors(node).reverse();
+					for (let index: number = 0; index < ancestors.length; index += 1) {
+						const ancestor: Deno.lint.Node = ancestors[index];
+						const ancestorOffset1: Deno.lint.Node | undefined = ancestors[index + 1];
+						const ancestorOffset2: Deno.lint.Node | undefined = ancestors[index + 2];
+						if (
+							ancestor.type === "ArrowFunctionExpression" ||
+							ancestor.type === "ClassBody" ||
+							ancestor.type === "ClassDeclaration" ||
+							ancestor.type === "ClassExpression" ||
+							ancestor.type === "FunctionDeclaration" ||
+							ancestor.type === "FunctionExpression" ||
+							ancestor.type === "Program" ||
+							ancestor.type === "TSClassImplements"
+						) {
+							break;
 						}
-					});
+						if (ancestor.type === "BlockStatement" && ancestorOffset1?.type === "FunctionExpression" && ancestorOffset2?.type === "MethodDefinition" && ancestorOffset2?.kind === "constructor") {
+							context.report({
+								node,
+								message: `Return value in the class constructor is possibly mistake.`,
+								fix(fixer: Deno.lint.Fixer): Deno.lint.Fix | Iterable<Deno.lint.Fix> {
+									return fixer.remove(node.argument!);
+								}
+							});
+						}
+					}
 				}
 			}
 		};
