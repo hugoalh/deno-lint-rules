413609987822498b7c474d710256c37c9a508160
hugoalh <hugoalh@gmail.com>
2025-05-17 13:31:38 +0800

    Improve rule `no-useless-expression`

--- _rules/no_useless_expression.ts
+++ _rules/no_useless_expression.ts
@@ -1,17 +1,42 @@
-import type { DenoLintRuleData } from "../_utility.ts";
+import {
+	isNodeBooleanLiteral,
+	isNodeNoOperation,
+	type DenoLintRuleData
+} from "../_utility.ts";
 const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
 		return {
 			ExpressionStatement(node: Deno.lint.ExpressionStatement): void {
-				if ((
-					node.expression.type === "ArrowFunctionExpression" ||
-					node.expression.type === "Identifier" ||
-					node.expression.type === "Literal"
-				)) {
-					context.report({
+				let dispatch: boolean = false;
+				let removable: boolean = false;
+				switch (node.expression.type) {
+					case "ArrowFunctionExpression":
+						dispatch = true;
+						break;
+					case "ArrayExpression":
+					case "TemplateLiteral":
+						dispatch = isNodeNoOperation(node.expression);
+						break;
+					case "Identifier":
+						dispatch = true;
+						removable = true;
+						break;
+					case "Literal":
+						dispatch = true;
+						removable = isNodeBooleanLiteral(node.expression);
+						break;
+				}
+				if (dispatch) {
+					const report: Deno.lint.ReportData = {
 						node,
 						message: `The expression will do nothing, possibly missing the assignment or call.`
-					});
+					};
+					if (removable && context.sourceCode.getCommentsInside(node.expression).length === 0) {
+						report.fix = (fixer: Deno.lint.Fixer): Deno.lint.Fix | Iterable<Deno.lint.Fix> => {
+							return fixer.remove(node.expression);
+						};
+					}
+					context.report(report);
 				}
 			}
 		};
