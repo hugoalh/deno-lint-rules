188735aa175e30222664bd305691979b59682302
hugoalh <hugoalh@gmail.com>
2025-05-17 13:29:54 +0800

    Update dependencies

--- _rules/prefer_ascii_identifier.ts
+++ _rules/prefer_ascii_identifier.ts
@@ -1,4 +1,4 @@
-import { isStringASCIIPrintable } from "https://raw.githubusercontent.com/hugoalh/is-string-ascii-es/v1.1.4/printable.ts";
+import { isStringASCIIPrintable } from "https://raw.githubusercontent.com/hugoalh/is-string-ascii-es/v1.1.5/printable.ts";
 import type { DenoLintRuleData } from "../_utility.ts";
 const ruleContext: Deno.lint.Rule = {
 	create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
