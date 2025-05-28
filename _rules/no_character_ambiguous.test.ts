8f3454df164d0979ada7487a2a5f720c0c35987c
hugoalh <hugoalh@gmail.com>
2025-05-28 15:42:51 +0800

    Update test

---
+++ _rules/no_character_ambiguous.test.ts
@@ -0,0 +1,15 @@
+//deno-lint-ignore-file hugoalh/no-character-ambiguous
+import { deepStrictEqual } from "node:assert";
+import { ruleData } from "./no_character_ambiguous.ts";
+import { constructPlugin } from "../_utility.ts";
+const rule = constructPlugin({
+	[ruleData.identifier]: ruleData.context()
+});
+Deno.test("Invalid 1", { permissions: "none" }, () => {
+	const diagnostics = Deno.lint.runPlugin(rule, "foo.ts", `const foo = "𝟮𝟯";`);
+	deepStrictEqual(diagnostics.length, 2);
+});
+Deno.test("Valid 1", { permissions: "none" }, () => {
+	const diagnostics = Deno.lint.runPlugin(rule, "foo.ts", `const foo = "23";`);
+	deepStrictEqual(diagnostics.length, 0);
+});
