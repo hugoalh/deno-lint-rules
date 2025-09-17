9b6f0929a0ecbaac0af2764c040f858003092830
hugoalh <hugoalh@gmail.com>
2025-09-17 18:59:23 +0800

    Add rule `no-depend-registry-dlmr`

---
+++ _rules/no_depend_registry_dlmr.ts
@@ -0,0 +1,53 @@
+import {
+	isNodeStringLiteral,
+	type RuleData
+} from "../_utility.ts";
+function ruleAssertor(context: Deno.lint.RuleContext, source: Deno.lint.StringLiteral): void {
+	const sourceURL: URL | null = URL.parse(source.value);
+	if (sourceURL !== null &&
+		(
+			sourceURL.protocol === "http:" ||
+			sourceURL.protocol === "https:"
+		) && (
+			sourceURL.hostname === "deno.com" ||
+			sourceURL.hostname === "deno.land" ||
+			sourceURL.hostname === "www.deno.com" ||
+			sourceURL.hostname === "www.deno.land"
+		) && sourceURL.pathname.startsWith("/x/")
+	) {
+		context.report({
+			node: source,
+			message: `Deno Land Module Registry is deprecated and should not depend on it.`
+		});
+	}
+}
+export const ruleData: RuleData = {
+	identifier: "no-depend-registry-dlmr",
+	tags: [
+		"recommended"
+	],
+	querier(): Deno.lint.Rule {
+		return {
+			create(context: Deno.lint.RuleContext): Deno.lint.LintVisitor {
+				return {
+					ExportAllDeclaration(node: Deno.lint.ExportAllDeclaration): void {
+						ruleAssertor(context, node.source);
+					},
+					ExportNamedDeclaration(node: Deno.lint.ExportNamedDeclaration): void {
+						if (node.source !== null) {
+							ruleAssertor(context, node.source);
+						}
+					},
+					ImportDeclaration(node: Deno.lint.ImportDeclaration): void {
+						ruleAssertor(context, node.source);
+					},
+					ImportExpression(node: Deno.lint.ImportExpression): void {
+						if (isNodeStringLiteral(node.source)) {
+							ruleAssertor(context, node.source);
+						}
+					}
+				};
+			}
+		};
+	}
+};
