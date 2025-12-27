77799d04038dea0c3215d19571b5235dd18bcbc6
hugoalh <hugoalh@gmail.com>
2025-12-27 16:22:59 +0800

    Fmt

--- _rules/max_complexity.ts
+++ _rules/max_complexity.ts
@@ -12,14 +12,13 @@ export interface RuleMaxComplexityOptions {
 function ruleAssertor(options: Required<RuleMaxComplexityOptions>, context: Deno.lint.RuleContext, node: Deno.lint.Node): void {
 	const { maximum }: Required<RuleMaxComplexityOptions> = options;
 	let ancestors: Deno.lint.Node[] = [...context.sourceCode.getAncestors(node), node];
-	let complexity: number = ancestors.length;
 	const indexTSTypeAnnotation: number = ancestors.findIndex(({ type }: Deno.lint.Node): boolean => {
 		return (type === "TSTypeAnnotation");
 	});
 	if (indexTSTypeAnnotation >= 0) {
 		ancestors = ancestors.slice(indexTSTypeAnnotation);
-		complexity = ancestors.length;
 	}
+	let complexity: number = ancestors.length - 1;
 	for (let index: number = 0; index < ancestors.length; index += 1) {
 		const current: Deno.lint.Node = ancestors[index];
 		const parent: Deno.lint.Node | undefined = ancestors[index - 1];
@@ -82,7 +81,7 @@ function ruleAssertor(options: Required<RuleMaxComplexityOptions>, context: Deno
 			message: `Too complex; Maximum: ${maximum}, Current: ${complexity}.`,
 			hint: `Ancestors: ${ancestors.map(({ type }: Deno.lint.Node): string => {
 				return type;
-			}).join(", ")}`
+			}).join(" > ")}`
 		});
 	}
 }
