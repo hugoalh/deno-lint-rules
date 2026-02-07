d0660e605fb8cc212bf50767072f6ce879ad9af0
hugoalh <hugoalh@gmail.com>
2026-02-07 18:18:50 +0800

    Improve rule `fmt-jsdoc` submessages

--- _rules/fmt_jsdoc.ts
+++ _rules/fmt_jsdoc.ts
@@ -6,6 +6,48 @@ import {
 	type RuleData,
 	type VisualPosition
 } from "../_utility.ts";
+class FormatJSDocMessageService {
+	blockStartInNewLine: number = 0;
+	contentStartInNewLine: number = 0;
+	contentEndInNewLine: number = 0;
+	indent: number = 0;
+	conclusion(): string | undefined {
+		const count: number = this.blockStartInNewLine + this.contentStartInNewLine + this.contentEndInNewLine + this.indent;
+		if (count === 0) {
+			return;
+		}
+		const messages: string[] = [`JSDoc is not in well format [${count}]:`];
+		if (this.blockStartInNewLine > 0) {
+			let message: string = `- Should in new line`;
+			if (this.blockStartInNewLine > 1) {
+				message += ` * ${this.blockStartInNewLine}`;
+			}
+			messages.push(message);
+		}
+		if (this.contentStartInNewLine > 0) {
+			let message: string = `- Content should start with new line`;
+			if (this.contentStartInNewLine > 1) {
+				message += ` * ${this.contentStartInNewLine}`;
+			}
+			messages.push(message);
+		}
+		if (this.contentEndInNewLine > 0) {
+			let message: string = `- Content should end with new line`;
+			if (this.contentEndInNewLine > 1) {
+				message += ` * ${this.contentEndInNewLine}`;
+			}
+			messages.push(message);
+		}
+		if (this.indent > 0) {
+			let message: string = `- Line is not well indent`;
+			if (this.indent > 1) {
+				message += ` * ${this.indent}`;
+			}
+			messages.push(message);
+		}
+		return messages.join("\n");
+	}
+}
 export const ruleData: RuleData = {
 	identifier: "fmt-jsdoc",
 	tags: [
@@ -25,18 +67,14 @@ export const ruleData: RuleData = {
 								const position: VisualPosition = getVisualPositionFromNode(context, node);
 								const frontRaw: string = context.sourceCode.text.split("\n")[position.lineBegin - 1].slice(0, position.columnBegin - 1);
 								const isInNewLine: boolean = frontRaw.trim().length === 0;
-								const submessages: Set<string> = new Set<string>();
+								const messager: FormatJSDocMessageService = new FormatJSDocMessageService();
 								const fixers: ((fixer: Deno.lint.Fixer) => Deno.lint.Fix)[] = [];
-
-								// New Line
 								if (!isInNewLine) {
-									submessages.add(`JSDoc should in new line.`);
+									messager.blockStartInNewLine += 1;
 									fixers.push((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
 										return fixer.insertTextBefore(node, "\n");
 									});
 								}
-
-								// Indent
 								if (lines.length > 1) {
 									const frontIndent: string = `${isInNewLine ? frontRaw : ""} * `;
 									for (let index: number = 0; index < lines.length; index += 1) {
@@ -44,7 +82,7 @@ export const ruleData: RuleData = {
 										const frontRange: Deno.lint.Range = [line.raw.range[0], line.cooked.range[0]];
 										if (index === 0) {
 											if (line.cooked.value.trim().length !== 0) {
-												submessages.add(`JSDoc content should start with new line.`);
+												messager.contentStartInNewLine += 1;
 												fixers.push((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
 													return fixer.replaceTextRange(frontRange, `\n${frontIndent}`);
 												});
@@ -55,19 +93,19 @@ export const ruleData: RuleData = {
 											const frontIndentLast: string = frontIndent.slice(0, -2);
 											if (line.cooked.value.trim().length === 0) {
 												if (line.raw.value !== frontIndentLast) {
-													submessages.add(`JSDoc content should end with new line.`);
+													messager.contentEndInNewLine += 1;
 													fixers.push((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
 														return fixer.replaceTextRange(line.raw.range, frontIndentLast);
 													});
 												}
 											} else {
 												if (line.raw.value.slice(0, frontRange[1] - frontRange[0]) !== frontIndent) {
-													submessages.add(`JSDoc line is not well indent.`);
+													messager.indent += 1;
 													fixers.push((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
 														return fixer.replaceTextRange(frontRange, frontIndent);
 													});
 												}
-												submessages.add(`JSDoc content should end with new line.`);
+												messager.contentEndInNewLine += 1;
 												fixers.push((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
 													return fixer.replaceTextRange([line.cooked.range[1], node.range[1] - 2], `\n${frontIndentLast}`);
 												});
@@ -75,26 +113,27 @@ export const ruleData: RuleData = {
 											continue;
 										}
 										if (line.raw.value.slice(0, frontRange[1] - frontRange[0]) !== frontIndent) {
-											submessages.add(`JSDoc line is not well indent.`);
+											messager.indent += 1;
 											fixers.push((fixer: Deno.lint.Fixer): Deno.lint.Fix => {
 												return fixer.replaceTextRange(frontRange, frontIndent);
 											});
 										}
 									}
 								}
-
-								if (submessages.size > 0) {
-									context.report({
+								const message: string | undefined = messager.conclusion();
+								if (typeof message !== "undefined") {
+									const report: Deno.lint.ReportData = {
 										node,
-										message: `JSDoc is not in well format [${submessages.size}]:\n${Array.from(submessages.values()).map((submessage: string): string => {
-											return `- ${submessage}`;
-										}).join("\n")}`,
-										fix(fixer: Deno.lint.Fixer): Deno.lint.Fix | Iterable<Deno.lint.Fix> {
+										message
+									};
+									if (fixers.length > 0) {
+										report.fix = (fixer: Deno.lint.Fixer): Deno.lint.Fix | Iterable<Deno.lint.Fix> => {
 											return fixers.map((subfix: (fixer: Deno.lint.Fixer) => Deno.lint.Fix): Deno.lint.Fix => {
 												return subfix(fixer);
 											}).reverse();
-										}
-									});
+										};
+									}
+									context.report(report);
 								}
 							}
 						}
